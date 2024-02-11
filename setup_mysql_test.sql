fe271ea9a8ee5b82c90f0d6191b501fa7bf2710e
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-11 20:12:05 +0100

    Prepares mysql for test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- prepare MYSQL for testing
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost';
