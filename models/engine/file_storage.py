21cf4556d91740403fbbd74e7006c7694ac9e41f
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-19 20:51:23 +0100

    Added docstring to function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,6 +56,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
+        """Deletes an object from list of instances"""
         if obj is not None:
             delIns = f"{obj.__class__.__name__}.{obj.id}"
             del FileStorage.__objects[delIns]
