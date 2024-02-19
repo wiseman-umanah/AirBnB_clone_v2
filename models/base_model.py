07c09131176aa66b006c6124d9d669aced7c98b8
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-19 21:27:12 +0100

    Added column section of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,11 @@ Base = declarative_base()
 
 
 class BaseModel:
-	"""A base class for all hbnb models"""
-	id = Column(String(60), nullable=False, primary_key=True)
-	created_at = Column(DateTime, server_default=utcnow(), nullable=False)
-	updated_at = Column(DateTime, server_default=utcnow(), nullable=False)
+    """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, server_default=utcnow(), nullable=False)
+    updated_at = Column(DateTime, server_default=utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -20,7 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
+
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -38,7 +39,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-		storage.new(self)
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
