e6bda6ca7e1329c1d249c4d431f7b28f0ab3854b
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-19 21:25:43 +0100

    Added column section of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,17 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+	"""A base class for all hbnb models"""
+	id = Column(String(60), nullable=False, primary_key=True)
+	created_at = Column(DateTime, server_default=utcnow(), nullable=False)
+	updated_at = Column(DateTime, server_default=utcnow(), nullable=False)
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +38,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+		storage.new(self)
         storage.save()
 
     def to_dict(self):
