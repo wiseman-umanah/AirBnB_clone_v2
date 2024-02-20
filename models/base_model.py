941873d0c47d13ee2e91d4c80f273854c5343ae1
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-20 19:51:21 +0100

    Fixed instantiation logic

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,8 @@ from os import getenv
 db_type = getenv("HBNB_TYPE_STORAGE")
 if db_type == "db":
     Base = declarative_base()
+else:
+    Base = object
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -20,16 +22,21 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        if kwargs:
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
 
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            if 'created_at' not in kwargs:
+                self.created_at = self.updated_at = datetime.now()
         else:
-            for key, value in kwargs.keys():
-                setattr(self, key, value)
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
