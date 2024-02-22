edf339cf63da9fdb378c217e1528b8da00ea9ed5
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-22 16:43:39 +0100

    Relationship should work

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,12 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """Table for Amenities"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity,
+                                   back_populates="amenities")
