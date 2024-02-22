8b54fe5ade194885441a0cb717fc3f6016f66369
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-22 16:23:18 +0100

    table restructed

--- models/place.py
+++ models/place.py
@@ -6,9 +6,8 @@ from sqlalchemy.orm import relationship
 import models
 
 
-metadata = MetaData()
 place_amenity = Table(
-    "place_amenity", metadata,
+    "place_amenity", Base.metadata,
     Column("place_id", String(60), ForeignKey("places.id"),
            primary_key=True, nullable=False),
     Column("amenity_id", String(60), ForeignKey("amenities.id"),
